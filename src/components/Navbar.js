6c3b776d6c00684c419f418773abe00245576454
ednavho <ednaho121@gmail.com>
2023-11-30 21:09:42 -0800

    create app

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo">
+            ANML
+          </Link>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
